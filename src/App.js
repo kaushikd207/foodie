ee8d7e2233a1f84c0dd80ea469d93273a6c96ea3
kaushikd207 <kaushikd207@gmail.com>
2024-02-23 19:28:45 +0530

    added some cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-
+import { restaurants } from "./constants/global";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
 const Header = () => {
   return (
     <div className="navBar">
@@ -26,10 +27,46 @@ const Header = () => {
   );
 };
 
+const Card = ({ restaurant }) => {
+  const {
+    name,
+    locality,
+    areaName,
+    costForTwo,
+    cuisines,
+    avgRating,
+    cloudinaryImageId,
+  } = restaurant?.info;
+  const imgLink = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`;
+  return (
+    <>
+      <div className="card">
+        <div className="cardImage">
+          <img src={imgLink}></img>
+        </div>
+        <h2>{name}</h2>
+        <LocationCityIcon />
+        <span>
+          {locality},{areaName}
+        </span>
+        <h4>{cuisines.length > 1 ? cuisines.join(", ") : cuisines}</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{avgRating}</h4>
+      </div>
+    </>
+  );
+};
 function App() {
   return (
     <>
       <Header />
+      <div className="cardBody">
+        <div className="cardContainer">
+          {restaurants.map((restaurant) => {
+            return <Card restaurant={restaurant} />;
+          })}
+        </div>
+      </div>
     </>
   );
 }
